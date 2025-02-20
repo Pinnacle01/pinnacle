c319dee7c4b9096340160cbbd11bfa108f80d9de
VaishnavYash <y01vaishnava@gmail.com>
2025-02-20 23:24:30 +0530

    Add travel cards components and Flutter lint workflow

--- lib/framework_demo.dart
+++ lib/framework_demo.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:pinnacle_main/framework/constants/size.dart';
 import 'package:pinnacle_main/framework/constants/strings.dart';
-import 'package:pinnacle_main/framework/widgets/components.dart';
 import 'package:pinnacle_main/framework/widgets/navigation_bar/custom_bottom_navigation_bar.dart';
 import 'package:pinnacle_main/framework/widgets/notification_tab.dart';
+import 'package:pinnacle_main/framework/widgets/travel_cards/category_card.dart';
+import 'package:pinnacle_main/framework/widgets/travel_cards/travel_request_component.dart';
 import 'package:pinnacle_main/framework/widgets/user_profile/user_profile_card.dart';
 import 'package:pinnacle_main/framework/widgets/user_widgets.dart';
 import 'package:sizer/sizer.dart';
