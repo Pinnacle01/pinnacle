21fdacd298f6bf2cfe5cecbac14cb7032c561565
VaishnavYash <y01vaishnava@gmail.com>
2025-02-23 23:01:09 +0530

    Update import paths

--- lib/framework_demo.dart
+++ lib/framework_demo.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pinnacle_main/explore_home/uikit/neraby_attraction_small_card.dart';
+import 'package:pinnacle_main/explore_home/uikit/single_category_card.dart';
+import 'package:pinnacle_main/explore_home/uikit/travel_request_component.dart';
 import 'package:pinnacle_main/framework/constants/asset_path.dart';
 import 'package:pinnacle_main/framework/constants/color.dart';
 import 'package:pinnacle_main/framework/constants/strings.dart';
@@ -7,9 +10,6 @@ import 'package:pinnacle_main/framework/widgets/common_component/group_request_c
 import 'package:pinnacle_main/framework/widgets/navigation_bar/custom_bottom_navigation_bar.dart';
 import 'package:pinnacle_main/framework/widgets/navigation_bar/floating_action_button.dart';
 import 'package:pinnacle_main/framework/widgets/tool_bar_widgets.dart';
-import 'package:pinnacle_main/framework/widgets/travel_cards/neraby_attraction_small_card.dart';
-import 'package:pinnacle_main/framework/widgets/travel_cards/single_category_card.dart';
-import 'package:pinnacle_main/framework/widgets/travel_cards/travel_request_component.dart';
 import 'package:pinnacle_main/framework/widgets/user_profile/user_profile_card.dart';
 import 'package:pinnacle_main/framework/widgets/user_widgets.dart';
 
